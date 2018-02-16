2a4d75ce76f8094a5e8f9ef177b652e8ba411067
Phil Hawksworth <phil@netify.com>
2018-02-16 16:25:56 +0000

    clean accidental dist commits

--- src/js/linky.js
+++ src/js/linky.js
@@ -5,7 +5,7 @@ btn.addEventListener('click', function (event) {
   fetch('/.netlify/functions/generate-route?destination='+url)
   .then(function(response) { return response.json(); })
   .then(function(data) {
-    document.querySelector("#confirmation").innerHTML = '<a href="' + data.url + '">' + data.url + '</a>';
+    document.querySelector("#confirmation").innerHTML = '<a href="' + data.url + '" target="_BLANK" rel="noopener">' + data.url + '</a>';
     return;
   });
 }, false);
