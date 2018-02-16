7c49e89890db9ca8455ed0e655c85f0eb5b3d61c
Phil Hawksworth <phil@netify.com>
2018-02-16 18:54:16 +0000

    allow cleint side queries for a root

---
+++ src/lambda/get-route.js
@@ -0,0 +1,41 @@
+'use strict';
+
+var request = require("request");
+var config = require("dotenv").config();
+
+
+export function handler(event, context, callback) {
+
+  // get the details of what we are creating
+  var code = event.queryStringParameters['code'];
+
+  // where is the data?
+  var url = "https://api.netlify.com/api/v1/forms/" + process.env.ROUTES_FORM_ID + "/submissions/?access_token=" + process.env.API_AUTH;
+  console.log("Requesting THIS", url);
+
+  request(url, function(err, response, body){
+
+    // look for this code in our stash
+    if(!err && response.statusCode === 200){
+      var roots = JSON.parse(body);
+      for(var item in roots) {
+        // return the result when we find the match
+        if(roots[item].data.code == code) {
+          console.log("We searched for " + code + " and we found " + roots[item].data.destination);
+          return callback(null, {
+            statusCode: 200,
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify({url: roots[item].data.destination})
+          })
+        }
+      }
+
+    } else {
+      return callback(null, {
+        statusCode: 200,
+        body: err
+      })
+    }
+  });
+
+}
