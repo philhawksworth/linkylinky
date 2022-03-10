be98f4f4b499532c122726a641a551eca26cf4ce
Phil Hawksworth <phil@netlify.com>
2022-03-10 10:22:59 +0000

    avoid looping redirects of unfound routes

--- src/assets/js/linky.js
+++ src/assets/js/linky.js
@@ -3,8 +3,8 @@
 */
 function eventHandlers() {
   var btn = document.querySelector('#btn-create');
-  if(!btn){ return;}
-  btn.addEventListener('click', function (event) {
+  if (!btn) { return; }
+  btn.addEventListener('click', function(event) {
     event.preventDefault();
     submitURL();
   }, false);
@@ -19,11 +19,11 @@ function eventHandlers() {
 function submitURL() {
   var url = document.querySelector('#destination').value;
   fetch('/.netlify/functions/generate-route?to=' + url)
-  .then(function(response) { return response.json(); })
-  .then(function(data) {
-    document.querySelector("#message").innerHTML = `<a href="${data.url}">${data.url}</a>`;
-    return;
-  });
+    .then(function(response) { return response.json(); })
+    .then(function(data) {
+      document.querySelector("#message").innerHTML = `<a href="${data.url}">${data.url}</a>`;
+      return;
+    });
 }
 
 
@@ -34,16 +34,18 @@ function submitURL() {
 */
 function redirectIfRequired() {
   var path = document.location.pathname;
-  if(path !== "/") {
+  if (path == "undefined") {
+    document.location.href = "/";
+  }
+  if (path !== "/") {
     document.querySelector('#message').innerHTML = "The redirect rules for that short URL is still being created... sending you directly!";
-    fetch('/.netlify/functions/get-route?code='+path.replace("/",""))
-    .then(function(response) { return response.json(); })
-    .then(function(data) {
-      document.location.href = data.url;
-    });
+    fetch('/.netlify/functions/get-route?code=' + path.replace("/", ""))
+      .then(function(response) { return response.json(); })
+      .then(function(data) {
+        document.location.href = data.url;
+      });
   }
 }
-
 
 eventHandlers();
 redirectIfRequired();
